65aa0fa89f50cb93377cd927a9ebdc28dd9287a3
pahalce <peaceee.programming@gmail.com>
2026-08-29 15:45:36 +0900

    feat: enable typed routes and Cache Components

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  typedRoutes: true,
+  cacheComponents: true,
 };
 
 export default nextConfig;
